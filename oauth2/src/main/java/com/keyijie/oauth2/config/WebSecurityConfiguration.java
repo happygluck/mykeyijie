b9a1015d5a0e4d5c43f7deff556a4ab49af3b489
LiuTao <happygluck@live.cn>
2018-03-24 16:09:50 +0800

    update5

--- oauth2/src/main/java/com/keyijie/oauth2/config/WebSecurityConfiguration.java
+++ oauth2/src/main/java/com/keyijie/oauth2/config/WebSecurityConfiguration.java
@@ -1,17 +1,28 @@
 package com.keyijie.oauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Web安全配置
  */
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
 
     @Override
     @Bean
@@ -21,9 +32,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("liutao")
-                .password("123456").authorities("FOO_READ");
+        auth.userDetailsService(userDetailsService())
+                //.passwordEncoder(new Md5PasswordEncoder())
+                .and()
+                .eraseCredentials(false);// 禁止清除登录凭证
     }
 
     @Override
---
+++ oauth2/src/main/java/com/keyijie/oauth2/service/UserDetailsServiceImpl.java
@@ -0,0 +1,69 @@
+package com.keyijie.oauth2.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 用户信息服务
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    /**
+     * 获取用户信息
+     *
+     * @param username 用户登录名
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username == null || username.trim().equals(""))
+            throw new UsernameNotFoundException("用户名或密码错误");
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                Set<GrantedAuthority> grantedAuthoritys = new HashSet<GrantedAuthority>();
+
+                return grantedAuthoritys;
+            }
+
+            @Override
+            public String getPassword() {
+                return "";
+            }
+
+            @Override
+            public String getUsername() {
+                return username;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+    }
+}
