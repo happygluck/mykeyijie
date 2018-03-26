38d064129d770cdf40017810bbcc2191f65cae8b
LiuTao <happygluck@live.cn>
2018-03-26 23:54:22 +0800

    添加服务注册

--- service-order/src/main/java/com/keyijie/order/OrderApplication.java
+++ service-order/src/main/java/com/keyijie/order/OrderApplication.java
@@ -2,9 +2,11 @@ package com.keyijie.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.keyijie")
 public class OrderApplication {
 
